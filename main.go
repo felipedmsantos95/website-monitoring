45c8efc9da3b5b76701812a7fa78f3cb26d6fa80
Felipe Santos <fdms@icomp.ufam.edu.br>
2023-04-07 21:43:15 -0400

    reading sites from file and saving logs

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
+
+const numberOfChecks = 3
+const delay = 5
 
 func main() {
 
@@ -20,7 +29,7 @@ func main() {
 		case 1:
 			startMonitoring()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			printLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -55,12 +64,85 @@ func readCommand() int {
 func startMonitoring() {
 	fmt.Print("\033[H\033[2J")
 	fmt.Println("Monitorando...")
-	site := "https://random-status-code.herokuapp.com/"
-	resp, _ := http.Get(site)
+	sites := readSitesFromFile()
+
+	for i := 0; i < numberOfChecks; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testSite(site)
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println()
+	}
+
+	fmt.Println()
+
+}
+
+func testSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		logRegister(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		logRegister(site, true)
 	}
+
+}
+
+func readSitesFromFile() []string {
+	var sites []string
+	file, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		sites = append(sites, line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	file.Close()
+
+	return sites
+}
+
+func logRegister(site string, status bool) {
+	file, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site +
+		" - online: " + strconv.FormatBool(status) + "\n")
+
+	file.Close()
+
+}
+
+func printLogs() {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("Exibindo Logs...")
+	file, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(file))
 }
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Website Monitoring
+## About
+
+A Golang application to monitore websites health.
+
+
+## Requirements
+
+To execute the project it is necessary to have the following requirements installed in the system:
+
+- [Go](https://go.dev/dl/)
+
+## Running the project
+
+### Cloning the project
+
+```bash
+$ git clone https://github.com/felipedmsantos95/website-monitoring
+$ cd website-monitoring
+$ go run main.go
+```
+
